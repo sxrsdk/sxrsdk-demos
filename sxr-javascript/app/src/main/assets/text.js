a4bd96fa5c1340d241717c25ec0362d1727e9bc9
Tom Flynn <tom.flynn@samsung.com>
2018-10-26 13:19:12 -0700

    gvr to sxr in the javascript files
    gvr to sxr in the javascript files

--- sxr-javascript/app/src/main/assets/text.js
+++ sxr-javascript/app/src/main/assets/text.js
@@ -1,4 +1,4 @@
-importPackage(org.gearvrf)
+importPackage(com.samsungxr)
 
 var colors = [0xFFFFFF, 0xE53935, 0xD81B60, 0x8E24AA, 0x5E35B1, 0x3949AB, 0x00897B];
 var colorIdx = 0;
@@ -6,7 +6,7 @@ var colorIdx = 0;
 var sceneObject = null;
 var localAnimation = -1;
 
-function onInit(gvrf, sceneObj) {
+function onInit(sxr, sceneObj) {
     sceneObject = sceneObj;
 }
 
