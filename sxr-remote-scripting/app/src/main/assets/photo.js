f5d7a0acebd12a4143a20fed679d36e689bae192
Tom Flynn <tom.flynn@samsung.com>
2018-11-05 11:00:05 -0800

    updating from gvr to sxr
    updating from gvr to sxr

--- sxr-remote-scripting/app/src/main/assets/photo.js
+++ sxr-remote-scripting/app/src/main/assets/photo.js
@@ -1,12 +1,12 @@
-importPackage(org.gearvrf);
-importPackage(org.gearvrf.nodes);
+importPackage(com.samsungxr);
+importPackage(com.samsungxr.nodes);
 
-var url = new java.net.URL("https://raw.githubusercontent.com/gearvrf/GearVRf-Demos/master/gvr-360photo/app/src/main/res/raw/photosphere.jpg");
-var resource = new GVRAndroidResource(gvrf, url);
-var texture = gvrf.getAssetLoader().loadTexture(resource);
-var photosphere = new GVRSphereNode(gvrf, false, texture);
+var url = new java.net.URL("https://raw.githubusercontent.com/sxrsdk/sxrsdk-demos/master/sxr-360photo/app/src/main/res/raw/photosphere.jpg");
+var resource = new SXRAndroidResource(sxr, url);
+var texture = sxr.getAssetLoader().loadTexture(resource);
+var photosphere = new SXRSphereNode(sxr, false, texture);
 photosphere.getTransform().setScale(20, 20, 20);
 photosphere.setName("photosphere");
-var scene = gvrf.getMainScene();
+var scene = sxr.getMainScene();
 scene.addNode(photosphere);
 
--- sxr-remote-scripting/app/src/main/assets/video.js
+++ sxr-remote-scripting/app/src/main/assets/video.js
@@ -1,12 +1,12 @@
-importPackage(org.gearvrf);
-importPackage(org.gearvrf.nodes);
+importPackage(com.samsungxr);
+importPackage(com.samsungxr.nodes);
 
-var sphere = new GVRSphereNode(gvrf, false);
+var sphere = new SXRSphereNode(sxr, false);
 var mesh = sphere.getRenderData().getMesh();
-var mediaPlayer = org.gearvrf.utility.ImageUtils.createMediaPlayer("https://github.com/gearvrf/GearVRf-Demos/raw/master/gvr-360video/app/src/main/assets/video.mp4");
-var video = new GVRVideoNode(gvrf, mesh, mediaPlayer, GVRVideoNode.GVRVideoType.MONO );
+var mediaPlayer = com.samsungxr.utility.ImageUtils.createMediaPlayer("https://github.com/sxrsdk/sxrsdk-demos/raw/master/sxr-360video/app/src/main/assets/video.mp4");
+var video = new SXRVideoNode(sxr, mesh, mediaPlayer, SXRVideoNode.SXRVideoType.MONO );
 video.setName("video");
-var scene = gvrf.getMainScene();
+var scene = sxr.getMainScene();
 scene.addNode(video);
 
 mediaPlayer.start();
