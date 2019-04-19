7165669438064ed793214937ac64679ad87c6e80
Mihail Marinov <m.marinov@samsung.com>
2019-04-19 12:39:30 -0700

    sxr-arcore: correctly request permissions and respond to the result

--- sxr-arcore/app/src/main/java/com/samsungxr/arcore/simplesample/SampleActivity.java
+++ sxr-arcore/app/src/main/java/com/samsungxr/arcore/simplesample/SampleActivity.java
@@ -15,12 +15,16 @@
 
 package com.samsungxr.arcore.simplesample;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 
 import com.samsungxr.SXRActivity;
 import com.samsungxr.utility.Log;
 
-public class SampleActivity extends SXRActivity {
+public class SampleActivity extends SXRActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
     private static final String TAG = "SXR_ARCORE";
     private SampleMain mMain;
 
@@ -30,6 +34,38 @@ public class SampleActivity extends SXRActivity {
         enableGestureDetector();
         Log.d(TAG, "onCreate");
         mMain = new SampleMain();
-        setMain(mMain, "sxr.xml");
+
+        final int camera = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        final int readPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        if (PackageManager.PERMISSION_GRANTED != camera || PackageManager.PERMISSION_GRANTED != readPermission) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA},
+                    REQUEST_PERMISSIONS);
+        } else {
+            setMain(mMain, "sxr.xml");
+        }
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if (0 == grantResults.length) {
+            return;
+        }
+        switch (requestCode) {
+            case REQUEST_PERMISSIONS:
+            {
+                if (PackageManager.PERMISSION_GRANTED != grantResults[1]) {
+                    Log.e(TAG, "Camera permission not granted");
+                    finish();
+                }
+                break;
+            }
+        }
+
+        setMain(mMain);
+    }
+
+    private final static int REQUEST_PERMISSIONS = 1;
+
 }
