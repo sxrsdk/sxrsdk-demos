a526c89a079ca76b14ed5517a8bc8d58ee6af629
tom.flynn <tom.flynn@samsung.com>
2019-11-05 11:10:38 -0800

    improve AA, replace background with cubemap
    A couple of minor changes:
     - Increased the number of multisamples
     - Tweaked the layouts a bit to reduce aliasing.
     - Replaced the sphere background with a nicer, higher-res cubemap
    
    Still checking on whether we can use that cubemap, or if we have to
    replace it with another one.

--- sxr-videoplayer/app/src/main/java/com/samsungxr/videoplayer/VideoPlayerApp.java
+++ sxr-videoplayer/app/src/main/java/com/samsungxr/videoplayer/VideoPlayerApp.java
@@ -69,7 +69,7 @@ public class VideoPlayerApp extends Application {
             Map<String, Typeface> map = (Map<String, Typeface>) fieldValue;
             map.put(fontFamily, typeface);
         } catch (Exception e) {
-            Log.d(TAG, e.getMessage());
+            Log.d(TAG, "failed to inject custom typeface.  message: " + e.getMessage());
         }
     }
 
